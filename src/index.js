6603da36fe08771e1800dba3bad5e814e2674422
arshad <arshadbasha.g@gmail.com>
2023-08-08 07:38:44 +0530

    11. Cookies

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express=require("express")
+const cookieParser = require('cookie-parser');
 const todosRouter=require("./routes/todos/todos");
 const app=express()
 const PORT=3000
@@ -13,6 +14,32 @@ const myMiddleware = function(req, res, next) {
   
 // Using the custom middleware
 app.use(myMiddleware);
+
+
+
+// Middleware to parse cookies
+app.use(cookieParser());
+
+// Route to set a cookie
+app.get('/set-cookie', (req, res) => {
+  // Set a cookie named "username" with value "john"
+  res.cookie('username', 'john', { maxAge: 900000, httpOnly: true }); // maxAge is in milliseconds
+  res.send('Cookie has been set.');
+});
+
+// Route to retrieve a cookie
+app.get('/get-cookie', (req, res) => {
+  // Retrieve the value of the "username" cookie
+  const username = req.cookies.username;
+  if (username) {
+    res.send(`Welcome back, ${username}!`);
+  } else {
+    res.send('No username cookie found.');
+  }
+});
+
+
+
 
 // Mount the todoRouter at '/todos' as a middleware
 app.use("/todos",todosRouter);
