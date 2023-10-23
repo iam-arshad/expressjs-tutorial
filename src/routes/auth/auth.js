e9ebe88441a1e1536d7a5d276a06d7d0f988ae92
iam-arshad <arshadbasha.g@gmail.com>
2023-10-23 17:21:30 +0530

    22a. Protected routes using JWT

--- src/routes/auth/auth.js
+++ src/routes/auth/auth.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 const User = require("../../database/schemas/users");
 const { hashPassword, setLoggedInUserAndCookie } = require("../../utils/authUtils");
 const passport = require("passport");
+const jwt = require("jsonwebtoken")
 const router = Router();
 
 // prevent user from get,post methods of register,login(but not profile,logout) routes if already logged in
@@ -56,10 +57,15 @@ router.get("/login", isAlreadyLoggedIn, (req, res) => {
 
 
 // POST:http://localhost:3000/auth/login
-router.post('/login', isAlreadyLoggedIn, passport.authenticate("local", { successRedirect: "/todos", failureRedirect: "/auth/login" }));
+router.post('/login', isAlreadyLoggedIn, passport.authenticate("local", { failureRedirect: "/auth/login" }), (req, res) => {
+  // User authenticated, generate a JWT
+  const user = req.user; // Assuming you have the user object in the request
+  const accessToken = jwt.sign({ id: user.id, username: user.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+  res.json({ token: accessToken });
+});
 
 // google oauth2 authentication route
-router.get("/google",passport.authenticate("google",{ scope: ['profile', 'email'] }))
+router.get("/google", passport.authenticate("google", { scope: ['profile', 'email'] }))
 
 // Google OAuth 2.0 callback URL
 router.get('/api/google/redirect', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
@@ -68,9 +74,27 @@ router.get('/api/google/redirect', passport.authenticate('google', { failureRedi
   res.redirect('/auth/profile');
 });
 
+// middleware to verify the accessToken
+function verifyToken(req, res, next) {
+  if (!req.isAuthenticated()) {
+    return res.status(401).send("You're not logged in")
+  }
+
+  // 'Bearer eyJhzVCJ9.eyJpZCI6IjY1M.CkuDkZYapgF'
+  const accessToken = req.headers["authorization"].split(" ")[1];
+  jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, data) => {
+    if (err) {
+      return res.status(401).json({ msg: "invalid token" }); //Unauthorized
+    }
+    // passport's local strategy will set the user's data in request object by default after logging in
+    // so, i am not utilizing the data(id,username,...) present in jwt here.
+    next();
+  })
+}
+
 // GET:http://localhost:3000/auth/profile
-router.get('/profile', (req, res) => {
-  res.send(`Welcome to your profile, ${req.user}`);
+router.get('/profile', verifyToken, (req, res) => {
+  res.send(`Welcome to your profile, ${req.user.username}`);
 });
 
 
